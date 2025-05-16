5c79b8c5d6b672bfa7a69381f56fdeb7b5c2232b
kangbohyeon <qhgus535@naver.com>
2025-05-16 23:36:08 +0900

    first commit

---
+++ src/test/kotlin/com/bohyun/portfolio/PortfolioApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bohyun.portfolio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
